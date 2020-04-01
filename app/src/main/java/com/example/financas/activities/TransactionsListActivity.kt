039c7a6c7e7fc5b82c6ab3cc545e5fe423bc23f5
Mateus Souza <mateus.souza@dmcard.com.br>
2020-04-01 14:35:26 -0300

    Calculando e exibindo despesa na tela

--- app/src/main/java/com/example/financas/activities/TransactionsListActivity.kt
+++ app/src/main/java/com/example/financas/activities/TransactionsListActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.financas.R
+import com.example.financas.activities.Extension.formatBrazilianCurrency
 import com.example.financas.activities.adapter.TransactionAdapter
 import com.example.financas.activities.model.TransacionType
 import com.example.financas.activities.model.Transaction
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import kotlinx.android.synthetic.main.activity_lista_transacoes.view.*
+import kotlinx.android.synthetic.main.resumo_card.*
 import java.math.BigDecimal
 import java.util.Calendar
 
@@ -24,5 +26,19 @@ class TransactionsListActivity: AppCompatActivity() {
         )
 
         lista_transacoes_listview.adapter = TransactionAdapter(transactionsList, this)
+
+        resumo_card_receita.text = calculateIncome(transactionsList).formatBrazilianCurrency()
+    }
+
+    private fun calculateIncome(transactions: List<Transaction>): BigDecimal{
+        var totalIncome = BigDecimal.ZERO
+
+        for (transaction in transactions){
+            if (transaction.type == TransacionType.INCOME){
+                totalIncome = totalIncome.plus(transaction.valor)
+            }
+        }
+
+        return totalIncome
     }
 }
